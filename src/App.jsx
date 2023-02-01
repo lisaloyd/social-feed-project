c7ecd33d5e1c82234570c77c7904f4c6da592937
unknown <greateducation123@yahoo.com>
2023-02-01 08:45:01 -0600

    Modified the Post.jsx component + created a PostButton.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,25 @@ import React, { useState } from "react";
 import PostList from "./Components/PostList/PostList";
 import CreatePostForm from "./Components/CreatePostForm/CreatePostForm";
 
+
+
+
 function App() {
   const [entries, setEntries] = useState([
     { name: "Lisa Loyd", post: " I will be there!" },
   ]);
-  
-    function addNewEntry(entry) {
-      let tempEntries = { entry, ...entries };
-      setEntries(tempEntries);
-    }
 
-    return (
-      <div>
-        <CreatePostForm AddNewEntryProperty={addNewEntry} />
-        <PostList parentEntries={entries} />
-      </div>
-    );
-  
+  function addNewEntry(entry) {
+    let tempEntries = { entry, ...entries };
+    setEntries(tempEntries);
+  }
+
+  return (
+    <div>
+      <CreatePostForm AddNewEntryProperty={addNewEntry} />
+      <PostList parentEntries={entries} />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -13,4 +13,5 @@ const Post = props => {
     );
 }
 
+
 export default Post;
